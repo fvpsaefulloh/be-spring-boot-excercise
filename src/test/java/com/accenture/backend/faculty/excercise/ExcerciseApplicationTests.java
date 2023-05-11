347c52afdc8bd7fc1ef6d0259d5201de0a141243
fivip.saefulloh <fvpsaefulloh@gmail.com>
2023-05-11 14:23:30 +0700

    first commit

---
+++ src/test/java/com/accenture/backend/faculty/excercise/ExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.backend.faculty.excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
